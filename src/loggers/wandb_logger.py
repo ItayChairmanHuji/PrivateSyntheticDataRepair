0e464cf8a0bf68d18d900fe2ee5cc056ea901aa8
Itay <test@test.com>
2026-02-23 20:22:27 +0200

    Logger config

--- src/loggers/wandb_logger.py
+++ src/loggers/wandb_logger.py
@@ -5,12 +5,9 @@ from omegaconf import DictConfig, OmegaConf
 
 
 class WANDBLogger:
-    def __init__(self, project_name: str, entity: str, run_name: str, metric: str, configuration=None):
+    def __init__(self, project_name: str, entity: str, run_name: str, configuration=None):
         config_dict = self._get_config(configuration)
-
-        self.run = self._create_run(project_name, entity, run_name, metric, config_dict)
-        self.run.define_metric(metric)
-        self.run.define_metric("*", step_metric=metric)
+        self.run = self._create_run(project_name, entity, run_name, config_dict)
 
     @staticmethod
     def _get_config(configuration):
@@ -18,12 +15,11 @@ class WANDBLogger:
             if isinstance(configuration, DictConfig) else dict(configuration)
 
     @staticmethod
-    def _create_run(project_name: str, entity: str, run_name: str, metric: str, config_dict: dict):
+    def _create_run(project_name: str, entity: str, run_name: str, config_dict: dict):
         return wandb.init(
             project=project_name,
             entity=entity,
             name=run_name,
-            group=metric,
             config=config_dict,
             reinit="create_new",
         )
